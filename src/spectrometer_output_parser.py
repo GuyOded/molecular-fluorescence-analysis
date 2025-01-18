8e46d1f7734ff4637050db4ee6d8ed900d0e8a20
Guy Oded <guy@somemail.com>
2025-01-18 17:06:24 +0200

    Add plots for all the Fluor's data

--- src/spectrometer_output_parser.py
+++ src/spectrometer_output_parser.py
@@ -17,7 +17,7 @@ def parse_excel_output(output_path: Path, default_sample_time=None) -> Spectrome
     # Remove empty columns
     dataframe = dataframe.loc[:, ~dataframe.columns.str.contains("^Unnamed")]
     try:
-        sample_time = dataframe.loc[0, 2]
+        sample_time = dataframe.iloc[0, 2]
     except IndexError as e:
         if default_sample_time == None:
             print("Cannot find parse sample time, please provide a default or fix data")
@@ -27,13 +27,14 @@ def parse_excel_output(output_path: Path, default_sample_time=None) -> Spectrome
         sample_time = default_sample_time
 
     # The code assumes the wavelength data is in the
-    wavelength_to_intensity_dataframe = pd.DataFrame({"wavelength": dataframe[:, 0], "intensity": dataframe[:, 1]})
+    wavelength_to_intensity_dataframe = pd.DataFrame({"wavelength": dataframe.iloc[:, 0], "intensity": dataframe.iloc[:, 1]})
 
     return SpectrometerOutput(wavelength_to_intensity_dataframe, sample_time, output_path)
 
 
 def parse_excels_in_folder(folder_path: Path, default_sample_time=1000) -> list[SpectrometerOutput]:
-    if not Path(folder_path).is_dir():
+    folder_path = Path(folder_path)
+    if not folder_path.is_dir():
         raise ValueError(f"Path \"{folder_path}\" does not lead to a directory")
 
-    return [SpectrometerOutput(out, default_sample_time) for out in os.listdir(folder_path) if Path(out).suffix == ".ods"]
+    return [parse_excel_output(out) for out in folder_path.iterdir() if Path(out).suffix == ".ods"]
